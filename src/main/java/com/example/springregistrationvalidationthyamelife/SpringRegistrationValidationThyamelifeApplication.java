c3f3c0ccd9f59f0ddab856aac05a7e55548a9f9d
Sahak-S <sahak.sa@mail.ru>
2023-02-24 23:03:06 +0400

    Initial commit

---
+++ src/main/java/com/example/springregistrationvalidationthyamelife/SpringRegistrationValidationThyamelifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springregistrationvalidationthyamelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistrationValidationThyamelifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRegistrationValidationThyamelifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springregistrationvalidationthyamelife/SpringRegistrationValidationThyamelifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springregistrationvalidationthyamelife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegistrationValidationThyamelifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
